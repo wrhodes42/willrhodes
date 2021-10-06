010a58d8ea0044c2085aed83a24c8aea2231cd0e
Will Rhodes <wrhodes42@gmail.com>
2021-10-06 13:38:03 -0400

    just do it static

--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1><a href="https://www.sanity.io/">Power your VueJS apps with a CMS</a></h1>
-    <h2>Learn how to integrate Sanity, The Ultimate Content Platform with your VueJS App.</h2>
-  </div>
-</template>
