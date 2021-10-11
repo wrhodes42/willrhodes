cb653cb0df717098e1db80bfec799e4ec9f7754a
Will Rhodes <wrhodes42@gmail.com>
2021-10-11 17:20:46 -0400

    project template, started on slick slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
+
+
+
 $(function(){
 
-    //initializations
-    $('.header').marquee();
+
+    $('#menu').accordion({
+        heightStyle: 'content',
+        icons: false,
+        collapsible: true,
+        active: false
+    });
+
+    $('#header').marquee({
+        speed: 20,
+        gap: 0,
+        delayBeforeStart: 0,
+        duplicated: true,
+        startVisible: true,
+        pauseOnHover: true,
+        pauseOnCycle: false
+    });
+
+
+    $('.project-slider').slick({
+
+    });
 
 })
