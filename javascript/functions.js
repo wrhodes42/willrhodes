ecd14ce1e65f89d62f757f238832e7fbb50605e7
Will Rhodes <wrhodes42@gmail.com>
2025-06-22 14:13:37 -0400

    content, marquees

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,30 @@
+$(document).ready(function(){
+  UpdateProjectInfo();
+});
+
+function UpdateProjectInfo(){
+    var activeSection = $('.section.active');
+    
+    projectInfoObjects.forEach(function(obj){
+        if(activeSection.attr("id") == obj.projectID){
+          
+          $('.project-title').each(function(){
+            $(this).html(obj.projectTitle);
+          });
+
+          $('.project-year').each(function(){
+            $(this).html(obj.projectYear);
+          });
+
+          $('.project-link').each(function(){
+            $(this).html(obj.projectLink);
+          });
+          
+          $('#project-marquee').marquee('start');
+        }
+    })
+}
+
 var Fullpage = new fullpage
 (
     '#fullpage', 
@@ -7,10 +34,33 @@ var Fullpage = new fullpage
         controlArrows: true,
         slidesNavigation: false,
         continuousVertical: true,
+        scrollOverflow: false,
         resetSliders: true,
         resetSlidersKey: 'd2lsbHJob2Rlcy5ibHVlXzhuV2NtVnpaWFJUYkdsa1pYSnpXcks='
     }
 );
+
+$('#contact-marquee').marquee({
+  speed: 40,
+  gap: 0,
+  delayBeforeStart: 0,
+  direction: 'left',
+  duplicated: true,
+  duplicateCount: 6,
+  pauseOnHover: true,
+  startVisible: true
+});
+
+$('#project-marquee').marquee({
+  speed: 40,
+  gap: 0,
+  delayBeforeStart: 0,
+  direction: 'left',
+  duplicated: true,
+  duplicateCount: 6,
+  pauseOnHover: true,
+  startVisible: true
+});
 
 $(document).on('click', '#move-up', function(){
   fullpage_api.moveSectionUp();
